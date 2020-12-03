bfdb24b55f645fd21545529e38d4e159d11927b7
24167hsrw <74053276+24167hsrw@users.noreply.github.com>
2020-12-03 12:22:19 +0100

    Update Course.java

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,1 +1,40 @@
-enum Course { ME, MSE, EL, IE, BMS, SCB, MME, MBB }
+public class Course {
+
+    private String id, name;
+    private Professor lecturer;
+    private int credit;
+
+    private Course(){
+        
+    }
+    public Course(String id, String name, Professor lecturer, int credit){
+        this.id= id;
+        this.name= name;
+        this.lecturer= lecturer;
+        this.credit= credit;
+    }
+    public String getID(){
+        return this.id;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public void setLecturer(Professor newProf){
+        this.lecturer= newProf;
+    }
+    public Professor getLecturer(){
+        return this.lecturer;
+    } 
+    public int getCredit(){
+        return this.credit;
+    }
+    public void setCredit(int newCredit){
+        this.credit= newCredit;
+    }
+
+    
+    
+}
